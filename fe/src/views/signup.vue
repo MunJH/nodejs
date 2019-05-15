1bf6b5582dce7062145a01fcfeb4c3d94f8fe9c1
Mangbo <whdudghks45629@gmail.com>
2019-05-15 12:03:57 +0900

    front signup.vue
    modify front

---
+++ fe/src/views/signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fill-height>
+    <v-layout row wrap align-center>
+      <v-flex xs12>
+        <v-toolbar flat>
+       </v-toolbar>
+        <v-card>
+          <div class="pa-3">
+            <v-text-field
+                v-model="email"
+                label="임시id"
+
+            >
+            </v-text-field>
+            <v-text-field
+                v-model="password"
+                label="임시pw"
+                type="password"
+            >
+            </v-text-field>
+            <v-btn
+             large
+             block
+             color="primary"
+             @click="login()"
+            >회원가입</v-btn>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
